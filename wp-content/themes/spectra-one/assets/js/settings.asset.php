c0aa0a172cb632febaf94bf7300c67e826180f3c
Raziyah Mwihaki <raziyahmwihaki@gmail.com>
2026-07-14 12:10:06 +0300

    feat: add patient dashboard page structure and spectra theme assets

---
+++ wp-content/themes/spectra-one/assets/js/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-site', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'd607989756050583f1b3');
